584d8ccf16523f658bec3f3bd510b19d6785f98a
Bùi Ngọc Lâm <74352628+ngoclam9200@users.noreply.github.com>
2021-10-03 17:41:52 +0700

    add new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { LayoutComponent } from './layout/layout.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    LayoutComponent,
+    SignupComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    RouterModule.forRoot([
+      {path:'signin', component:SigninComponent },
+      {path:'signup', component:SignupComponent },
+      {path:'home', component:HomeComponent },
+      {path:'aboutus', component:AboutusComponent },
+      {path:'contactus', component:ContactusComponent },
+      {path:'', component:LayoutComponent }
+    ])
+  
+
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/contactus/contactus.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contactus',
+  templateUrl: './contactus.component.html',
+  styleUrls: ['./contactus.component.css'],
+  
+})
+
+export class ContactusComponent implements OnInit {
+  myDate = Date.now();
+  constructor() {
+    
+   }
+
+  ngOnInit(): void {
+    
+  }
+
+}
